e1ed947a7591dbaf79b511bbca14767c437c2d86
Sahdev Rathod <sahdevrathod2020@gmail.com>
2026-04-25 12:46:57 +0530

    Consumed Api Using Ajax Call In javascriptProduct Controller (#5)
    Consumed Api Using Ajax Call In javascriptProduct Controller

---
+++ MVCClient/Scripts/sites/JsGetProduct.js
@@ -0,0 +1,22 @@
+﻿    $(function () {
+        $.ajax({
+            url: "https://localhost:44393/api/product",
+            type: "GET",
+            success: function (response) {
+                $('#AllProducts').DataTable({
+                    data: response,
+                    columns: [
+                        { data: 'id' },
+                        { data: 'name' },
+                        { data: 'price' },
+                        { data: 'stock' }
+                    ]
+                });
+
+                console.log("Products fetched successfully:", response);
+            },
+            error: function (error) {
+                console.error("Error fetching products:", error);
+            }
+        });
+    });
--- ProductApiDemo1/App_Start/WebApiConfig.cs
+++ ProductApiDemo1/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace ProductApiDemo1
 {
@@ -11,6 +12,7 @@ namespace ProductApiDemo1
         {
             // Web API configuration and services
 
+            config.EnableCors(new EnableCorsAttribute("*", "*", "*"));
             // Web API routes
             config.MapHttpAttributeRoutes();
 
